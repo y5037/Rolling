e689a28b4ef0e8ed3d56d5c9de8f06c3003ecd9a
parksangseok <>
2024-12-12 17:18:28 +0900

    feat:1.OutlinedButton 공통 컴포넌트 생성완료 2.GlobalStyle 컴포넌트 gray 변수명 100~900 적용

---
+++ src/components/OutlinedButton.jsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+const buttonStyles = {
+  '56px': {
+    minWidth: '192px',
+    fontWeight:600
+  },
+  '40px': {
+    minWidth: '122px',
+  },
+  '36px': {
+    minWidth: '122px',
+  },
+  '28px': {
+    minWidth: '122px',
+    fontSize: '14px',
+  },
+};
+
+const OutlinedButton = styled.button`
+
+  margin: 15px 0;
+  padding: 0 10px;
+  min-Width: ${(props) => buttonStyles[props.height]?.minWidth || '192px'}; 
+  height: ${(props) => props.height || '56px'};
+  font-size: ${(props) => buttonStyles[props.height]?.fontSize || '16px'};
+  border-radius: 6px;
+  border: 1px solid var(--Gray300);
+  background-color: var(--White);
+  font-weight: ${(props) => buttonStyles[props.height]?.fontWeight || '400'};
+  color: var(--Gray900);
+  font-size: 16px;
+  transition: all .3s;
+  
+  &:hover {
+
+  background-color: var(--Gray100);
+
+  }
+
+  &:disabled{
+  
+  border: 0;
+  background-color: var(--Gray300);
+  color: var(--White);
+  cursor: auto;
+
+  }
+
+  &:focus{
+  
+  outline: none;
+  border: 1px solid var(--Gray500);
+  
+  }
+
+`;
+
+export default OutlinedButton;
