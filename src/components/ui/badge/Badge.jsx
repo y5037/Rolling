35f43074f83bd00637b82d774a4a697633703424
강동우 <ghkjiop92@naver.com>
2024-12-13 15:13:24 +0900

    feat:공통 컴포넌트 뱃지 구현

--- src/components/ui/badge/Badge.jsx
+++ src/components/ui/badge/Badge.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const StyledBadge = styled.div`
   color: var(--Beige500);
   background-color: var(--Beige100);
-  padding: 5px 20px;
+  padding: 4px 20px;
   border-radius: 12px;
   font-size: 14px;
   font-weight: bold;
