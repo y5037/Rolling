26cfc9760575360d4a41b9df17b7f7e7fa03a487
강동우 <ghkjiop92@naver.com>
2024-12-13 14:36:40 +0900

    feat: 뱃지 컴포넌트 추가

---
+++ src/components/ui/badge/Badge.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const StyledBadge = styled.div`
+  color: var(--Beige500);
+  background-color: var(--Beige100);
+  padding: 5px 20px;
+  border-radius: 12px;
+  font-size: 14px;
+  font-weight: bold;
+  display: inline-block;
+  text-align: center;
+`;
+
+function Badge() {
+  return <StyledBadge>지인</StyledBadge>; // 예시 텍스트
+}
+
+export default Badge;
