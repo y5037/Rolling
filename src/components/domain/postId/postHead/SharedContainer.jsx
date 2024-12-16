6b2ef1d61dd3314c1059e0410b5530839058ce09
Nerim <nerim5037@gmail.com>
2024-12-16 18:40:53 +0900

    feat: URL 공유 클릭 시 현재 위치한 url 복사 기능 추가

--- src/components/domain/postId/postHead/SharedContainer.jsx
+++ src/components/domain/postId/postHead/SharedContainer.jsx
@@ -1,10 +1,15 @@
 import styles from "./PostHead.module.css";
 
 function SharedContainer() {
+  const currentUrl = window.location.href;
+  const handleCopyUrl = () => {
+    navigator.clipboard.writeText(currentUrl);
+  };
+
   return (
     <ul className={styles.sharedContainer}>
       <li>카카오톡 공유</li>
-      <li>URL 공유</li>
+      <li onClick={handleCopyUrl}>URL 공유</li>
     </ul>
   );
 }
