724f8e5ac57cddaf2d8e178ccb82d93bf9ece731
parkingun <engi9364@naver.com>
2024-12-17 15:47:42 +0900

    chore: 불필요한 파일 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/main/MainPage";
+import PostPage from "./pages/post/PostPage";
 import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/post" element={<PostPage />} />
         </Routes>
       </BrowserRouter>
     </React.StrictMode>
