4d7e47ea7c556a494e9e647e7faf22a342a6880a
parkingun <engi9364@naver.com>
2024-12-17 11:20:27 +0900

    fix: CreatePost 파일 conflict 해결

--- src/components/domain/post/CreatePost.jsx
+++ src/components/domain/post/CreatePost.jsx
@@ -10,27 +10,16 @@ function CreatePost() {
 
   const [name, setName] = useState("");
   const [backgroundOption, setBackgroundOption] = useState([]);
-<<<<<<< HEAD
   const [backgroundValue, setBackgroundValue] = useState([]);
-=======
-  const [backgroundValue, setBackgroundValue] = useState(colors);
->>>>>>> 6419a54d (fix: Navigation hide 처리 누륵으로 인한 작업)
 
   const handleNameChange = (e) => {
     setName(e.target.value);
   };
 
-<<<<<<< HEAD
   // const clickBackgroundOption = async (e) => {
   //   setBackgroundOption(e.target.value);
   //   const setBackgroundOption = await GetBackgoundImages();
   // };
-=======
-  const clickBackgroundOption = async (e) => {
-    setBackgroundOption(e.target.value);
-    // const setBackgroundOption = await GetBackgoundImages();
-  };
->>>>>>> 6419a54d (fix: Navigation hide 처리 누륵으로 인한 작업)
 
   // const GetBackgoundValue = () => {
   //   setBackgroundValue(backgroundOption);
@@ -41,7 +30,6 @@ function CreatePost() {
     console.log(name);
   };
 
-<<<<<<< HEAD
   // useEffect(() => {
   //   if (backgroundOption === "color") {
   //     setBackgroundValue(colors);
@@ -49,15 +37,6 @@ function CreatePost() {
   //     setBackgroundValue(images);
   //   }
   // }, [backgroundOption]);
-=======
-  useEffect(() => {
-    if (backgroundOption === "color") {
-      setBackgroundValue(colors);
-    } else if (backgroundOption === "image") {
-      setBackgroundValue(images);
-    }
-  }, [backgroundOption]);
->>>>>>> 6419a54d (fix: Navigation hide 처리 누륵으로 인한 작업)
 
   return (
     <form className="Container" onSubmit={handleSubmit}>
@@ -79,57 +58,30 @@ function CreatePost() {
         </div>
         <div className="SelectBackground">
           <button
-<<<<<<< HEAD
             className="ColorBackgroundBtn"
             value="color"
             type="button"
 
             // onClick={clickBackgroundOption}
-=======
-            className="ColorBackground"
-            value="color"
-            type="button"
-            onClick={clickBackgroundOption}
->>>>>>> 6419a54d (fix: Navigation hide 처리 누륵으로 인한 작업)
           >
             컬러
           </button>
           <button
-<<<<<<< HEAD
             className="ImageBackgroundBtn"
             value="image"
             type="button"
             // onClick={clickBackgroundOption}
-=======
-            className="ImageBackground"
-            value="image"
-            type="button"
-            onClick={clickBackgroundOption}
->>>>>>> 6419a54d (fix: Navigation hide 처리 누륵으로 인한 작업)
           >
             이미지
           </button>
         </div>
         <div className="BackgoundOption">
-<<<<<<< HEAD
           {colors.map((color) => (
             <PostBackground key={color} option="color" value={color} />
           ))}
         </div>
         <div className="CreateButton">
           <button className="SubmitBtn" type="submit" disabled={!name.trim()}>
-=======
-          {backgroundValue.map((value) => (
-            <PostBackground
-              key={value}
-              option={backgroundOption}
-              value={value}
-            />
-          ))}
-        </div>
-        <div className="CreateButton">
-          <button className="Submit" type="submit" disabled={!name.trim()}>
->>>>>>> 6419a54d (fix: Navigation hide 처리 누륵으로 인한 작업)
             생성하기
           </button>
         </div>
