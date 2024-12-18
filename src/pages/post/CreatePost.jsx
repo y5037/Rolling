c52f7a253f84308cc4e6c0e331db2bbde49d379d
parkingun <engi9364@naver.com>
2024-12-18 15:10:40 +0900

    feat: 롤링페이퍼 생성 페이지 추가

---
+++ src/pages/post/CreatePost.jsx
@@ -0,0 +1,13 @@
+import Navigation from "../../components/ui/nav/Navigation";
+import CreatePost from "../../components/domain/post/CreatePost";
+
+function PostPage() {
+  return (
+    <>
+      <Navigation hide />
+      <CreatePost />
+    </>
+  );
+}
+
+export default PostPage;
