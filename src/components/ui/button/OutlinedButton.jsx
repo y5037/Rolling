e68dbb0f70b1cb59c53aff2160ee8600237503b0
HyeyeonJung <jhy20306@gmail.com>
2024-12-19 22:33:35 +0900

    feat: button 트랜지션 통일 수정

--- src/components/ui/button/OutlinedButton.jsx
+++ src/components/ui/button/OutlinedButton.jsx
@@ -1,59 +1,51 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const buttonStyles = {
-  '56px': {
-    minWidth: '192px',
-    fontWeight:600
+  "56px": {
+    minWidth: "192px",
+    fontWeight: 600,
   },
-  '40px': {
-    minWidth: '122px',
+  "40px": {
+    minWidth: "122px",
   },
-  '36px': {
-    minWidth: '122px',
+  "36px": {
+    minWidth: "122px",
   },
-  '28px': {
-    minWidth: '122px',
-    fontSize: '14px',
+  "28px": {
+    minWidth: "122px",
+    fontSize: "14px",
   },
 };
 
 const OutlinedButton = styled.button`
-
   margin: 15px 0;
   padding: 0 10px;
-  min-Width: ${(props) => buttonStyles[props.height]?.minWidth || '192px'}; 
-  height: ${(props) => props.height || '56px'};
-  font-size: ${(props) => buttonStyles[props.height]?.fontSize || '16px'};
+  min-width: ${(props) => buttonStyles[props.height]?.minWidth || "192px"};
+  height: ${(props) => props.height || "56px"};
+  font-size: ${(props) => buttonStyles[props.height]?.fontSize || "16px"};
   border-radius: 6px;
   border: 1px solid var(--Gray300);
   background-color: var(--White);
-  font-weight: ${(props) => buttonStyles[props.height]?.fontWeight || '400'};
+  font-weight: ${(props) => buttonStyles[props.height]?.fontWeight || "400"};
   color: var(--Gray900);
   font-size: 16px;
-  transition: all .3s;
-  
+  transition: all 0.1s ease;
+
   &:hover {
-
-  background-color: var(--Gray100);
-
+    background-color: var(--Gray100);
   }
 
-  &:disabled{
-  
-  border: 0;
-  background-color: var(--Gray300);
-  color: var(--White);
-  cursor: auto;
-
+  &:disabled {
+    border: 0;
+    background-color: var(--Gray300);
+    color: var(--White);
+    cursor: auto;
   }
 
-  &:focus{
-  
-  outline: none;
-  border: 1px solid var(--Gray500);
-  
+  &:focus {
+    outline: none;
+    border: 1px solid var(--Gray500);
   }
-
 `;
 
 export default OutlinedButton;
