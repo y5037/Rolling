fc6649710113dde894c006ae1fb669447d7aeecd
Nerim <nerim5037@gmail.com>
2024-12-22 14:19:50 +0900

    chore: 이모지 count 출력으로 수정

--- src/components/domain/postId/postHead/EmojiContainer.jsx
+++ src/components/domain/postId/postHead/EmojiContainer.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import styles from "./PostHead.module.css";
 import PostReactions from "../../../../services/PostReactions";
+import GetReactions from "../../../../services/GetReactions";
 
 function EmojiContainer({
   emojiRef,
@@ -12,10 +13,29 @@ function EmojiContainer({
 
   const [isReaction, setIsReactions] = useState();
   const [countBadge, setCountBadge] = useState();
+  const [isEmoji, setIsEmoji] = useState();
 
   const handleEmojiClick = (i) => {
     setIsReactions(emojiButton[i]);
   };
+
+  const handleGetCount = async (options) => {
+    try {
+      const response = await GetReactions(options);
+      const { results } = response;
+      setIsEmoji(results);
+      console.log(results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetCount({
+      userId,
+      isCount: isEmoji,
+    });
+  }, [isReaction]);
 
   const handleUpdate = async (options) => {
     try {
@@ -35,6 +55,8 @@ function EmojiContainer({
       });
   }, [isReaction]);
 
+  console.log(isEmoji);
+
   return (
     <ul className={styles.emojiContainer} ref={emojiRef}>
       {emojiButton.map((item, i) => {
@@ -49,14 +71,24 @@ function EmojiContainer({
             }}
           >
             <p className={styles.emoji}>{item}</p>
-            {countBadge === i && (
-              <>
-                {/* 해당 key값은 리렌더링을 위한 임시 값 */}
-                <div key={isReaction} className={styles.fadeBadge}>
+            {/* {countBadge === i && (
+              <> */}
+            {/* 해당 key값은 리렌더링을 위한 임시 값 */}
+            {/* <div key={isReaction} className={styles.fadeBadge}>
                   {countRendering?.count}
-                </div>
-              </>
-            )}
+                </div> */}
+            {/* </> */}
+            {/* )} */}
+            {isEmoji &&
+              isEmoji.map((emoji) => {
+                return (
+                  <>
+                    {item === emoji.emoji && (
+                      <p key={emoji?.id}>{emoji.count}</p>
+                    )}
+                  </>
+                );
+              })}
           </li>
         );
       })}
--- src/services/GetRecipientsId.jsx
+++ src/services/GetRecipientsId.jsx
@@ -1,8 +1,9 @@
 import { API_URL } from "../constant/VariableSettings";
 
 // 특정 ID 데이터 불러오기
-async function GetRecipientsId(userId, setData) {
+async function GetRecipientsId(userId, setIsData) {
   const response = await fetch(`${API_URL}/12-4/recipients/${userId}/`);
+
 
   if (!response.ok) {
     throw new Error("유저 정보를 가져오는데 실패했습니다.");
@@ -11,7 +12,7 @@ async function GetRecipientsId(userId, setData) {
   const responseBody = await response.json();
   console.log("Response 데이터 형식은? ", typeof responseBody);
 
-  setData(responseBody);
+  setIsData(responseBody);
 }
 
 export default GetRecipientsId;
